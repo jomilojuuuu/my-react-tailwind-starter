594cb4b3895eab2752a5484eefdfa1d718576176
Jomiloju Oke <jomilojuforlife2004@gmail.com>
2023-04-20 23:15:49 -0800

    done with the design✌

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,28 +1,29 @@
-import React from 'react'
+import React from "react";
 
 function Form() {
   return (
-    <div className=''>
-        <form className='flex flex-col justify-center items-center mt-[90px]'>
-            <input 
-                type="text" 
-                placeholder="Title"
-                className='p-3 bg-red-300 w-80'
-            />
+    <div className="">
+      <form className="flex flex-col justify-center items-center mt-[90px]">
+        <input
+          type="text"
+          placeholder="Title"
+          className="p-3 bg-red-300 w-80 pl"
+        />
 
-            <input 
-                type="text" 
-                placeholder="What to do"
-                className='p-3 bg-pink-300 w-80'
-            />
-            <button type="submit" 
-            className="h-10 rounded-full bg-blue-300 text-white font-bold px-5 ml-[255px] -mt-5">
-                Add
-            </button>
-        </form>
+        <input
+          type="text"
+          placeholder="What to do"
+          className="p-3 bg-pink-300 w-80"
+        />
+        <button
+          type="submit"
+          className="h-10 rounded-full bg-blue-300 text-white font-bold px-5 ml-[255px] -mt-5"
+        >
+          Add
+        </button>
+      </form>
     </div>
-    
-  )
+  );
 }
 
-export default Form
+export default Form;
