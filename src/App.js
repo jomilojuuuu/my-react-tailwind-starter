d3f421e0019d955846d0ac1ebdf6d1d5abf2f9ec
Jomiloju Oke <jomilojuforlife2004@gmail.com>
2023-04-23 02:17:09 -0800

    added the delete function✌

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
-import React, {useState} from 'react'
-import Form from './components/Form'
-import Header from './components/Header'
-import Card from './components/Card'
+import React, { useState } from "react";
+import Form from "./components/Form";
+import Header from "./components/Header";
+import Card from "./components/Card";
 
 export default function App() {
+  const [cards, setCards] = useState([
+    {
+      title: "sleep",
+      whatToDo: "i need to sleep by 5pm",
+      id: 1,
+    },
+    {
+      title: "play",
+      whatToDo: "i need to play by 8am",
+      id: 2,
+    },
+    {
+      title: "read",
+      whatToDo: "i need to read by 12pm",
+      id: 3,
+    },
+  ]);
+
+  const handelDelete = (id) => {
+    const newCards = cards.filter((cards) => cards.id !== id);
+    setCards(newCards);
+  };
+
   return (
     <div>
       {/* header */}
@@ -12,7 +35,11 @@ export default function App() {
         <Form />
       </div>
       {/* card */}
-      <Card />
+      <div className="flex">
+        {cards.map((todo) => (
+          <Card key={cards.id} cards={todo} handelDelete={handelDelete} />
+        ))}
+      </div>
     </div>
-  )
+  );
 }
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,16 +1,25 @@
-import React from 'react'
+import React from "react";
 
-function Card() {
+function Card({ cards, handelDelete }) {
+  // const cards = props.todo;
+  const { whatToDo, title } = cards;
+
   return (
     <div>
-      <div className='box-border shadow-md border w-40 h-auto ml-10 mt-20 bg-gray-100'>
-        <p className='text-gray-400 text-lg font-bold'>what i want to do today </p>
-        <p className='text-gray-400 pt-2'>kiss</p>
-        <button className='bg-blue-200 ml-[88px] mt-1 px-3 shadow-md'>Delete</button>
-      </div>
-      
+      {
+        <div className="box-border shadow-md border w-40 h-auto ml-10 mt-20 bg-gray-100">
+          <p className="text-gray-400 text-lg font-bold px-2">{title}</p>
+          <p className="text-gray-400 pt-2 px-2">{whatToDo}</p>
+          <button
+            className="bg-blue-200 ml-[88px] mt-1 px-3 shadow-md"
+            onClick={() => handelDelete(cards.id)}
+          >
+            Delete
+          </button>
+        </div>
+      }
     </div>
-  )
+  );
 }
 
-export default Card
+export default Card;
